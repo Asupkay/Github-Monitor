15ce1aa5406607bb6069894ffdc3d264d57df22f
Alex Supkay <asupkay1124@gmail.com>
2018-08-19 14:35:20 -0400

    Update client and server also paste in github query

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+  
+  componentDidMount() {
+    this.callApi();
+  }
+  
+  callApi = async () => {
+    const response = await axios.get('/github');
+    console.log(response);
+    const message = response.data.message;
+    this.setState({response: message});
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +28,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          { this.state.response }
         </p>
       </div>
     );
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,7 @@
 const constructorMethod = (app) => {
+  app.use('/github', (req, res) => {
+    res.json({message: 'Github'});
+  });
   app.use('/', (req, res) => {
     res.json({message: 'Not yet implemented'});
   });
@@ -8,5 +11,51 @@ const constructorMethod = (app) => {
     });
   });
 }
+
+/*
+{
+  viewer {
+    repositories(first: 50) {
+      nodes{
+        id
+        name
+        pushedAt
+        url
+        collaborators(first: 10) {
+          nodes{
+            name
+            avatarUrl
+          }
+          totalCount
+        }
+        defaultBranchRef{
+          target{
+            ... on Commit{
+              history(first: 1) {
+                nodes{
+                  additions
+                  deletions
+                  author{
+                    name
+                    avatarUrl
+                  }
+                  pushedDate
+                  message
+                }
+                totalCount
+              }
+              
+            }
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+}
+*/   
 
 module.exports = constructorMethod;
