2b0da7124b7e299a11529ee26c96bf3c222de6a4
Alex Supkay <asupkay1124@gmail.com>
2018-08-21 00:24:00 -0400

    Make sort bar smaller

--- client/src/components/sortDropDown.js
+++ client/src/components/sortDropDown.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const SortDropDown = props => {
   return (
-    <select className="custom-select my-1 mr-sm-2" onChange={props.onChange} id="inlineFormCustomSelectPref" value={props.selected}>
-      <option value="alphabetical">Alphabetical</option>
-      <option value="lastCommit">Last Commit</option>
-      <option value="mostCommits">Most Commits</option>
-    </select>
+    <React.Fragment>
+      <p style={{display: 'inline'}}>Sort by: </p>
+      <select className="custom-select my-1 mr-sm-2" onChange={props.onChange} id="inlineFormCustomSelectPref" value={props.selected} style={{ width: '50%', display: 'inline'}} >
+        <option value="alphabetical">Alphabetical</option>
+        <option value="lastCommit">Last Commit</option>
+        <option value="mostCommits">Most Commits</option>
+      </select>
+    </React.Fragment>
   )   
 }
 
